3e0a6ff8f025aba82ce9a9a6f94249aa9aa6b558
CarlosArthurDev <103152440+CarlosArthurDev@users.noreply.github.com>
2022-07-23 22:54:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+menuListArray = ["Pizza vegetariana",
+"Pizza de Frango",
+"Pizza Portuguesa",
+"Pizza Quatro Queijos",
+"Pizza de Calabresa",
+"Pizza Extravaganza"
+]
+function getMenu(){
+var htmldata = "";
+
+}
+function addTop(){
+var item=document.getElementById("additem").value;
+menuListArray.push(item);
+addItem();
+}
